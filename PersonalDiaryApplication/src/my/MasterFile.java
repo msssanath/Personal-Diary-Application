70b445de17b972ea07e9d98872890ebafd55d7e5
pawancr7 <pterdal@gmail.com>
2018-04-14 13:32:56 +0530

    changed the structure of index files
    less complex now, removed maps and all that shit

--- PersonalDiaryApplication/src/my/MasterFile.java
+++ PersonalDiaryApplication/src/my/MasterFile.java
@@ -9,8 +9,7 @@ class FileStructure
   String name,time,title,content;                  // class variables
   String keywords;
   int count=0;                                     //no of records
-  public static TreeMap<String,String> primaryIndexmap = new TreeMap<String,String>();    // buffer for primaryIndex
-  public static TreeMap<String,String> secondaryIndexmap = new TreeMap<String,String>();  // buffer for secondaryIndex
+  
 
   public FileStructure(String name)
   {
@@ -66,12 +65,11 @@ class FileStructure
     {
       RandomAccessFile fout = new RandomAccessFile("MasterFile.txt","rw");
       fout.seek(fout.length());
-      long size = fout.getFilePointer();
-      //count++;
-      this.name = this.name + count;        //Primary Key format
-      fout.writeBytes(this.name+"|"+this.time+"|"+this.title+"|"+this.content+"|"+this.keywords+"\n");
-      writeToPrimaryIndexFile(this.name,size);
-      writeToSecondaryIndexFile(this.time,this.name);
+      long size = fout.getFilePointer();    
+             
+      fout.writeBytes(this.count+"|"+this.name+"|"+this.time+"|"+this.title+"|"+this.content+"|"+this.keywords+"\n");         //primary key format
+      writeToPrimaryIndexFile(String.valueOf(this.count),String.valueOf(size));
+      writeToSecondaryIndexFile(this.time,String.valueOf(this.count),this.name);
       fout.close();
     }
     catch(IOException e)
@@ -82,20 +80,15 @@ class FileStructure
 
   //***************************** enter details in primary index file ************************//
 
-  public void writeToPrimaryIndexFile(String name,long offset)
+  public void writeToPrimaryIndexFile(String primaryKey,String offset)
   {
     try
     {
-      readIndexFileContents("primary",primaryIndexmap);            // for sorting it
-      System.out.println(primaryIndexmap);
-      primaryIndexmap.put(name,String.valueOf(offset));
+      
       RandomAccessFile fout = new RandomAccessFile("PrimaryIndexFile.txt","rw");
-      for(String i : primaryIndexmap.keySet())
-      {
-        fout.writeBytes(i+"|"+primaryIndexmap.get(i)+"\n");
-      }
+      fout.seek(fout.length());
+      fout.writeBytes(primaryKey+"|"+offset+"\n");
       fout.close();
-
     }
     catch(IOException e)
     {
@@ -103,45 +96,15 @@ class FileStructure
     }
   }
 
-  //***************************** load the buffer with values ************************//
-  public void readIndexFileContents(String fileType,TreeMap<String,String> mapType)
-  {
-
-    try
-    {
-      RandomAccessFile fin;
-      String pairs[] = new String[2];
-      if(fileType.equals("primary"))
-        fin = new RandomAccessFile("PrimaryIndexFile.txt","r");
-      else
-        fin = new RandomAccessFile("SecondaryIndexFile.txt","r");
-      String line = fin.readLine();
-      while(line != null)
-      {
-        pairs = line.split("\\|");
-        mapType.put(pairs[0],pairs[1]);
-        line = fin.readLine();
-      }
-    }
-    catch(IOException e)
-    {
-      System.out.println("Error!");
-    }
-  }
 
   //************************ write into secondary index file ***************//
-  public void writeToSecondaryIndexFile(String date,String name)
+  public void writeToSecondaryIndexFile(String date,String primaryKey,String name)
   {
     try
     {
-      readIndexFileContents("secondary",secondaryIndexmap);
-      System.out.println(secondaryIndexmap);
-      secondaryIndexmap.put(date,name);
       RandomAccessFile fout = new RandomAccessFile("SecondaryIndexFile.txt","rw");
-      for(String i : secondaryIndexmap.keySet())
-      {
-        fout.writeBytes(i+"|"+secondaryIndexmap.get(i)+"\n");
-      }
+      fout.seek(fout.length());
+      fout.writeBytes(date+"|"+primaryKey+"|"+name+"\n");
       fout.close();
 
     }
@@ -156,8 +119,7 @@ class FileStructure
 class MasterFile
 {
   public static void main(String[] args) {
-    //Scanner in = new Scanner(System.in);
-    FileStructure user = new FileStructure("Sha");
+    //FileStructure user = new FileStructure("Sha");
     /*user.getUserDetails("Dead","Might die","Death,End");
     user.printUserDetails();
     user.writeToMasterFile();*/
